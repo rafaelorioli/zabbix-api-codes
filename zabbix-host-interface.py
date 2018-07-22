b63f2ff2f27cf6d2a9aaba1db52f5a7795418685
gmdutra <gabrieldmdutra@gmail.com>
2018-07-22 13:39:56 -0300

    zabbix search host description

---
+++ zabbix-host-interface.py
@@ -0,0 +1,25 @@
+from zabbix_api import ZabbixAPI
+
+zapi = ZabbixAPI(server="http://localhost/zabbix")
+zapi.login("admin","zabbix")
+
+hosts = zapi.host.get({
+	"output": [
+		"hostid",
+		"name"
+	]
+})
+
+interfaces = zapi.hostinterface.get({
+	"output":[
+		"hostid",
+		"ip",
+		"type"
+	]
+})
+
+for host in hosts:
+	for interface in interfaces:
+		if interface["hostid"] == host["hostid"]:
+			print("hostid: {0} - name: {1} - IP: {2} - Tipo de interface: {3}"\
+				.format(host["hostid"], host["name"], interface["ip"], interface["type"]))
