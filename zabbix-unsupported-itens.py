3af8079dcaf9a3e7ed0b7945d794577da9010dbe
gmdutra <gabrieldmdutra@gmail.com>
2018-07-22 15:34:06 -0300

    zabbix - search unsupported items

---
+++ zabbix-unsupported-itens.py
@@ -0,0 +1,20 @@
+from zabbix_api import ZabbixAPI
+
+zapi = ZabbixAPI(server="http://localhost/zabbix")
+zapi.login("admin","zabbix")
+
+itens = zapi.item.get({
+	"output": "extend",
+	"filter": {
+		"state": 1
+	}
+})
+print("===============================================")
+print("ID Host	-	ID Item 	-	Nome 	- 	Erro")
+print("===============================================")
+
+for item in itens:
+	print(item["hostid"], item["itemid"], item["name"], item["error"])
+
+print("===============================================")
+print("Total de itens nao suportados: {0}".format(len(itens)))
